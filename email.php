65d757469c46ab6c8c442adf352d5a8acc9db6ac
GauraVanam18 <132244932+GauraVanam18@users.noreply.github.com>
2023-10-27 09:16:57 +0530

    committed by gaurav

--- email.php
+++ email.php
@@ -15,7 +15,7 @@
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            // echo "Please complete the form and try again.";
             exit;
         }
         
